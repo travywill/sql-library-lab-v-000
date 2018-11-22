645a4a76ef4fd378d1843e9f9e203d0dfbd8937f
Travis Williamson <travywill@gmail.com>
2018-11-22 18:35:54 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,6 +36,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, 
-  FROM characters;"
+  "SELECT characters.name, authors.name
+  FROM characters
+  INNER JOIN characters ON authors.id = characters.author_id;"
 end
