d4829daa23848c83a0a044920954d734ebe5f125
Travis Williamson <travywill@gmail.com>
2018-11-22 18:47:06 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -38,5 +38,5 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, character_books.book_id
   FROM characters
-  INNER JOIN character_books ON character_books.character_id = character.id;"
+  INNER JOIN character_books ON character_books.character_id = characters.id;"
 end
