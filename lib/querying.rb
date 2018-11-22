b437128fa834f1d1207b37c3181c166791225255
Travis Williamson <travywill@gmail.com>
2018-11-22 18:33:54 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,6 +36,6 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, characters.motto
+  "SELECT characters.name
   FROM characters;"
 end
